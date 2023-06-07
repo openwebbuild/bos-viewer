873c5745dc9b3a1ee4e6ed5c3d178ebe21300683
Robert Yan <mstr.raphael@gmail.com>
2023-06-07 15:22:47 +0800

    chore: remove post-build script

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -8,8 +8,6 @@ export const NetworkId =
 export const signInContractId =
   NetworkId === "testnet" ? "v1.social08.testnet" : "social.near";
 
-console.log("current network", window.location.hostname, MainnetDomains, NetworkId, signInContractId);
-
 export const networks = {
   mainnet: {
     networkId: "mainnet",
